a84c0c022306c58d2b53506d77a6b1eec04a6c47
Sergey-Marchuk <53752286+Sergey-Marchuk@users.noreply.github.com>
2021-05-27 12:29:16 +0300

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,9 @@
 #include "factorial.hpp"
+#include "user_exception.hpp"
 int factorial(int n)
-{ if(n<0)
-    return -1;
+{
+  if(n<0)
+    throw UserException{};
   if(n == 0)
     return 1;
     return n * factorial(n-1);
